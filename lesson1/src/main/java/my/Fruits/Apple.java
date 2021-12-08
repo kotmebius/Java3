93e9862c6d9ccbc2832b140288a8ec4415b5535c
Nail.Khantemirov <kotmebius@gmail.com>
2021-12-09 00:15:27 +0500

    Первый урок

---
+++ lesson1/src/main/java/my/Fruits/Apple.java
@@ -0,0 +1,12 @@
+package my.Fruits;
+
+public class Apple extends Fruit{
+    String name = "Apple";
+    float weight=1.0f;
+
+    @Override
+    public float getWeight() {
+        return weight;
+    }
+
+}
